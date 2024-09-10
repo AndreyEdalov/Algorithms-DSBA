07a36843abf6a8f9ff4d53606e3f3f62957836a2
Max_Triber <a.edalov17@gmail.com>
2024-09-10 13:30:21 +0300

    Structure Update

---
+++ Sorting/merge_sort/Insertion_sort/Insertion_sort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && key < arr[j])
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+}
+
+void print_array(int arr[], int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    insertionSort(arr, n);
+    print_array(arr, n);
+
+    return 0;
+}
